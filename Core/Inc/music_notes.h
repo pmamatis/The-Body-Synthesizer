22ea925f1c065fe5ac9f49133f171edb3c035db9
Paul Emanuel Mamatis <p.mamatis@campus.tu-berlin.de>
2020-05-07 14:13:31 +0200

    signal synthesis start

---
+++ Core/Inc/music_notes.h
@@ -0,0 +1,36 @@
+/**
+  ******************************************************************************
+  * @file    music_notes.c
+  * @author  Max Lehmer,Paul Mamatis
+  * @date 	27 Apr 2020
+  * @brief	Funktionsgenerator für HAL-Libary auf Nucleo-144 F4.....
+  *
+  *
+  *
+*/
+
+
+#ifndef INC_MUSIC_NOTES_H_
+#define INC_MUSIC_NOTES_H_
+
+#include "main.h"
+
+
+enum chord_type{
+	fifth= 0,
+	seventh,
+	ninth,
+};
+
+enum sex_type{
+	major=0,
+	minor,
+	diminished
+};
+uint8_t Get_Keyindex(char key);
+void Play_Note(char note, uint8_t octave);
+uint32_t Get_Note_Frequency(uint8_t index,uint8_t octave);
+void Play_Chord(char root_note,uint8_t sex, uint8_t octave, uint8_t chordtype);
+
+
+#endif /* INC_MUSIC_NOTES_H_ */
