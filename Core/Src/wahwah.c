0f48fddf3dc2953eed3f4afdf6fb3692ad1a800c
Max <maximilian.lehmer@campus.tu-berlin.de>
2021-09-11 19:44:12 +0200

    cosmetics

--- Core/Src/wahwah.c
+++ Core/Src/wahwah.c
@@ -5,7 +5,7 @@
  * @date 	11 Sep 2021
  * @brief	WahWah-Library
  */
-
+//
 //#include "wahwah.h"
 //
 ///*********************************
@@ -17,25 +17,7 @@
 //	Tremolo.lfo_data = &lfo;
 //	SetupTremolo(&Tremolo);
 //
-//	return TREMOLO_OK;
-//}
-//
-//Tremolo_Status SetupTremolo(struct Tremolo_t* Tremolo) {
-//
-//
-//	Tremolo->lfo_data->lfo_index = 0;
-//	Tremolo->lfo_data->lfo_frequency = 8.00;	// was 4
-//	//Tremolo->lfo_frequency = 4.00;
-//	Tremolo->lfo_data->lfo_quarter = 0;
-//	Tremolo->lfo_data->lfo_blocksizecounter = 0;
-//	Tremolo->tremolo_blocksizecounter = 0;
-//	//Tremolo->lfo_depth = 0.00;
-//	Tremolo->lfo_data->lfo_depth = 1.0;	// was 0.5
-//	Tremolo->recalc_lfo = false;
-//	Tremolo->tremolo_maximum_rate = 9;
-//	Tremolo->tremolo_maximum_depth = 1.00;
-//
-//	return TREMOLO_OK;
+//	return WAHWAH_OK;
 //}
 //
 //
