1be93acda7f83ddf91e82cbc23cb69b221d3a7a6
Marc Bielen <marc.bielen@campus.tu-berlin.de>
2022-02-22 20:30:48 +0100

    updated Distortion_LUT.m

--- Matlab/Distortion_LUT.m
+++ Matlab/Distortion_LUT.m
@@ -6,10 +6,11 @@ max_distortion_gain = 10;
 values = linspace(-max_distortion_gain, max_distortion_gain, blocksize);
 aquidistance = (2*max_distortion_gain) / (blocksize-1);
 atan_LUT = atan(values);
-atan_LUT_rounded = round(atan_LUT,4);  % runden und transponieren
-tt = 1:blocksize;
+atan_LUT_rounded = round(atan_LUT,4);
 figure(1)
-plot(tt, atan_LUT_rounded); grid on
+plot(values, atan_LUT_rounded); grid on
+xlabel('Distortion Gain')
+ylabel('atan(x)')
 csvwrite('atan_LUT_y.txt',atan_LUT_rounded)
 csvwrite('atan_LUT_x.txt',values)
 dlmwrite('aquidistance.txt', aquidistance, 'delimiter', ',', 'precision', 15);
